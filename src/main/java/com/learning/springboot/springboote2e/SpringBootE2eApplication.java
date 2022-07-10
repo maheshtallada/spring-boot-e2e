d209ed610823017b827d591ca1c2da51d2c7ab6b
Mahesh Gupta Tallada <mahesh.tallada@hotmail.com>
2022-07-10 23:31:32 +0530

    First commit

---
+++ src/main/java/com/learning/springboot/springboote2e/SpringBootE2eApplication.java
@@ -0,0 +1,13 @@
+package com.learning.springboot.springboote2e;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootE2eApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootE2eApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/learning/springboot/springboote2e/SpringBootE2eApplicationTests.java
@@ -0,0 +1,13 @@
+package com.learning.springboot.springboote2e;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootE2eApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
