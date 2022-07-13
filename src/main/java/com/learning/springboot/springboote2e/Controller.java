d43ab94a25b7405e80100c6ceba2ef5bd25fe50c
Mahesh Gupta Tallada <mahesh.tallada@hotmail.com>
2022-07-13 14:05:32 +0530

    To add path variable to an uri and retrive it

--- src/main/java/com/learning/springboot/springboote2e/Controller.java
+++ src/main/java/com/learning/springboot/springboote2e/Controller.java
@@ -1,6 +1,7 @@
 package com.learning.springboot.springboote2e;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -9,12 +10,15 @@ public class Controller {
 	@GetMapping(path = "/welcome")
 	public String sendGreeting() {
 		return "Hello-World";
-		
 	}
 	
 	@GetMapping(path = "/welcome-bean")
 	public WelcomeBean sendGreetingBean() {
 		return new WelcomeBean("Hello World bean");
-		
+	}
+
+	@GetMapping(path = "/welcome/{phrase}")
+	public WelcomeBean sendGreetingPathVariable(@PathVariable(name = "phrase") String content) {
+		return new WelcomeBean("Hello " + content + "..!!");
 	}
 }
