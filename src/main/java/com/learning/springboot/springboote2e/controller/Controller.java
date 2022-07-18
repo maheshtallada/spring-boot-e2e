940eefca160b8a3bd6703fe490fa7c5c278e2836
Mahesh Gupta Tallada <mahesh.tallada@hotmail.com>
2022-07-18 22:03:02 +0530

    Internationalzation using MessageSource class and messages_<locale>.properties files

--- src/main/java/com/learning/springboot/springboote2e/controller/Controller.java
+++ src/main/java/com/learning/springboot/springboote2e/controller/Controller.java
@@ -1,7 +1,13 @@
 package com.learning.springboot.springboote2e.controller;
 
+import java.util.Locale;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.MessageSource;
+import org.springframework.context.i18n.LocaleContextHolder;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.learning.springboot.springboote2e.model.WelcomeBean;
@@ -9,6 +15,9 @@ import com.learning.springboot.springboote2e.model.WelcomeBean;
 @RestController
 public class Controller {
 	
+	@Autowired
+	private MessageSource messageSource;
+
 	@GetMapping(path = "/welcome")
 	public String sendGreeting() {
 		return "Hello-World";
@@ -22,5 +31,13 @@ public class Controller {
 	@GetMapping(path = "/welcome/{phrase}")
 	public WelcomeBean sendGreetingPathVariable(@PathVariable(name = "phrase") String content) {
 		return new WelcomeBean("Hello " + content + "..!!");
+	}
+
+	// internationalization - i18N
+	@GetMapping(path = "/welcome/i18n")
+	public String sendGreetingInternationalization(
+			//@RequestHeader(name = "Accept-Language", required=false) Locale locale
+			) {
+		return messageSource.getMessage("GM", null, LocaleContextHolder.getLocale());
 	}
 }
