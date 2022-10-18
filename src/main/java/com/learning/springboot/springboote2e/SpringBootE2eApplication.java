849e684193466fd55179a6d1faf19196f461adad
Mahesh Tallada <mahesh.tallada@gmail.com>
2022-10-18 18:23:02 +0530

    REST Principle - UniformInterface and sub principles of it

--- src/main/java/com/learning/springboot/springboote2e/SpringBootE2eApplication.java
+++ src/main/java/com/learning/springboot/springboote2e/SpringBootE2eApplication.java
@@ -2,9 +2,16 @@ package com.learning.springboot.springboote2e;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.client.RestTemplate;
 
 @SpringBootApplication
 public class SpringBootE2eApplication {
+	
+	@Bean
+	public RestTemplate getRestTemplate() {
+		return new RestTemplate();
+	}
 
 	public static void main(String[] args) {
 		SpringApplication.run(SpringBootE2eApplication.class, args);
